34cdd4b4ce3d86d5ea45f088c696c8a8d5358426
Walmqrt <46291857+Zenqrt@users.noreply.github.com>
2025-06-16 10:39:09 -0400

    Add servo stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,51 @@
 #include <Arduino.h>
+#include <Servo.h>
+
+#define SERVO_PIN 1
+#define GEAR_TEETH 12
+#define GEAR_PITCH 0.03 // meters
+
+const float DELTA_ANGLE = 360.0 / GEAR_TEETH;
+const float DELTA_HEIGHT = 2 * PI * (GEAR_PITCH / 2) / GEAR_TEETH;
+
+Servo servo;
+bool active = false;
+int heightAngle;
+
+void setContactState();
+void setSeparationState();
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(9600); // For debugging purposes
+
+  // Attach servo
+  servo.attach(SERVO_PIN);
+
+  // Reset servo to contact state
+  setContactState();
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+  if (!active && Serial.available())
+  {
+    String string = Serial.readString();
+
+    if (string.equalsIgnoreCase("start"))
+    {
+      active = true;
+    }
+    else if (string.startsWith("set:"))
+    {
+      int height = string.substring(4).toInt();
+    }
+
+    return;
+  }
+}
+
+void setContactState()
+{
+  servo.write(0);
 }
