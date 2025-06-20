607912ace694ddbaaca47f29df1f2ab4996c1b6e
Walmqrt <46291857+Zenqrt@users.noreply.github.com>
2025-06-20 19:22:05 -0400

    Reverse angle values and added "separate" and "contact" commands

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <Servo.h>
 #include "utils.h"
 
-#define SERVO_PIN 0
+#define SERVO_PIN 8
 
 #define GEAR_PITCH 30 // millimeters
 
@@ -21,7 +21,7 @@ void setup()
   servo.attach(SERVO_PIN);
 
   // Reset servo to contact state
-  servo.write(0);
+  servo.write(180);
 }
 
 void loop()
@@ -29,32 +29,52 @@ void loop()
   if (Serial.available())
   {
     const String string = Serial.readString();
-
+    Serial.println(string);
     // Start oscillation
-    if (!active && string.equalsIgnoreCase("start"))
+    if (!active)
     {
-      active = true;
-      Serial.println("Started oscillation");
+      if (string.equalsIgnoreCase("start"))
+      {
+        active = true;
+        Serial.println("Started oscillation");
+      }
+      else if (string.equalsIgnoreCase("separate"))
+      {
+        servo.write(100);
+      }
+      else if (string.equalsIgnoreCase("contact"))
+      {
+        servo.write(180);
+      }
+      else if (string.startsWith("set:"))
+      {
+        String *parameters = new String[2];
+        splitAtFirst(string.substring(4), ':', parameters);
+
+        const int height = parameters[0].toInt();
+        // const int height = 10;
+        heightAngle = 180 * height / (PI * GEAR_PITCH);
+
+        separationInterval = parameters[1].toInt();
+        // separationInterval = 1000;
+        writeDelay = (separationInterval / 2) / heightAngle;
+
+        Serial.println("Height:");
+        Serial.println(height);
+        Serial.println("Interval:");
+        Serial.println(separationInterval);
+
+        Serial.println("writeDelay:");
+        Serial.println(writeDelay);
+        }
     }
 
     // Stop oscillation
     else if (active && string.equalsIgnoreCase("stop"))
     {
       active = false;
+      servo.write(180);
       Serial.println("Stopped oscillation");
-    }
-
-    // Set parameters (should be done first)
-    else if (string.startsWith("set:"))
-    {
-      String *parameters = new String[2];
-      splitAtFirst(string.substring(4), ':', parameters);
-
-      const int height = parameters[0].toInt();
-      heightAngle = 180 * height / (PI * GEAR_PITCH);
-
-      separationInterval = parameters[1].toInt();
-      writeDelay = (separationInterval / 2) / heightAngle;
     }
     return;
   }
@@ -63,14 +83,14 @@ void loop()
   if (active)
   {
     // Separation
-    for (int angle = 0; angle <= heightAngle; angle++)
+    for (int angle = 180; angle >= 180 - heightAngle; angle--)
     {
       servo.write(angle);
       delay(writeDelay);
     }
 
     // Contact
-    for (int angle = heightAngle; angle >= 0; angle--)
+    for (int angle = 180 - heightAngle; angle <= 180; angle++)
     {
       servo.write(angle);
       delay(writeDelay);
